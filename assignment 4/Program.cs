d64676c0bf208e1ab2108b24d073f23d54035cb0
AHMED <AHMED@MSI>
2021-12-09 21:57:31 +0200

    Add project files.

---
+++ assignment 4/Program.cs
@@ -0,0 +1,10 @@
+﻿    int f = 1;
+    Console.Write("enter the value: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i <= num; i++)
+    {
+        f = f * i;
+    }
+    Console.WriteLine("Factorial of {0} is :{1}", num, +f);
+
+
